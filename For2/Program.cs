8612e93e98727a4f4a35d430033f690cbd8a1772
Estudiantes <Estudiantes@ULELAB331>
2023-09-15 16:04:53 -0500

    Agregar archivos de proyecto.

---
+++ For2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ingrese un numero limite: ");
+int limite = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= limite; i++)
+{
+    for (int j = 1; j <= i + 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
